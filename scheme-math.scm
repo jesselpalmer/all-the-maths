5db3c2d93768750ccec4b176331d0ceaead88f8f
Jesse Palmer <jesselpalmer@gmail.com>
2019-06-10 21:23:12 -0700

    refactor: rename function & add dr racket

--- scheme-math.scm
+++ scheme-math.scm
@@ -1,10 +1,13 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-advanced-reader.ss" "lang")((modname scheme-math) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #t #t none #f () #f)))
 (define PI 3.14159265359)
 (define EULERS_NUMBER 2.71828182845)
 
 (define (square n) 
   (* n n))
 
-(define (abs n)
+(define (absolute n)
   (if (< n 0)
     (- n)
     n))
